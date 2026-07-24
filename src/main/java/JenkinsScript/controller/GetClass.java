36f2939b6edb3e5c3c0faf60856f769a4fc0034b
NAVEEN RAJ <naveenthammisetty979@gmail.com>
2026-07-24 10:45:17 +0530

    SecondCommit

--- src/main/java/JenkinsScript/controller/GetClass.java
+++ src/main/java/JenkinsScript/controller/GetClass.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GetClass {
 
-	  @GetMapping("Fetch")
+	  @GetMapping("/Fetch")
 	  public String getMessge()
 	  {
 		  return "Data Fetched Successfully!!!!";
